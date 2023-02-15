281dea9b4524e397408846288a54c57d4a1739f6
sun <sunzhon>
2023-02-15 23:12:39 +0800

    wqupdate batch collect shell script

--- DeepDA/search_rotation_angle.sh
+++ DeepDA/search_rotation_angle.sh
@@ -4,40 +4,50 @@
 
 feature_layer_num=5 # keep it to use five. it is the best value
 sub_num=15
-train_sub_num=14
-trial_num=25
-for_num=0
+rot_id=0
 if true; then
-    for rotation_angle in $(seq -10 -10); do
-        for_num=`expr $for_num + 1`
+    for train_sub_num in 14; do
+        for trial_num in 25; do
+            for rot_angle in $(seq 5 5 30); do
+                rot_id=`expr $rot_id + $rot_angle`
+                rot_id=$rot_angle
 
-            if false; then
-                model_name="baseline_mlnn"
-                tre_data_relative_path="selection"
-            else
-                model_name="augmentation"
-                tre_data_relative_path="augmentation"
-            fi
+                if false; then
+                    model_name="baseline_mlnn"
+                    tre_data_relative_path="selection"
+                else
+                    model_name="augmentation"
+                    tre_data_relative_path="augmentation"
+                fi
 
-            config_id="v12_${rotation_angle}"
-            tst_data_relative_path="selection"
-            relative_result_folder="${model_name}_${config_id}"
-            base_name="kem_norm_landing_data.hdf5"
-            test_sub_num=2
-            #train_sub_num=$(expr $sub_num - $test_sub_num )
-            cv_num=8  #$train_sub_num
-            prefix_save_name="${for_num}_augmentation"
+                config_id="v14"
+                tst_data_relative_path="selection"
+                base_name="kem_norm_landing_data.hdf5"
 
-            echo "train ${model_name} with ${train_sub_num} subjects and ${trial_num} trials"
+                relative_result_folder="${model_name}_${config_id}"
 
-            python ./../vicon_imu_data_process/augmentation_data.py --xrot_angle 0 --yrot_angle 0 --zrot_angle $rotation_angle --prefix_save_name ${prefix_save_name}
+                #train_sub_num=$(expr $sub_num - $test_sub_num )
+                test_sub_num=2
+                cv_num=8  #$train_sub_num
 
-            python main.py --config run.yaml --model_selection ${model_name} --feature_layer_num ${feature_layer_num} --cv_num ${cv_num} --tre_domain "${tre_data_relative_path}/${prefix_save_name}_${trial_num}trials_${sub_num}subjects_${base_name}" --tst_domain "${tst_data_relative_path}/${tst_data_relative_path}_${trial_num}trials_${sub_num}subjects_${base_name}" --sub_num ${sub_num} --trial_num ${trial_num}  --train_sub_num "${train_sub_num}" --test_sub_num ${test_sub_num} --config_name "${train_sub_num}sub_${trial_num}trials_${model_name}_${config_id}" --relative_result_folder "${relative_result_folder}/${train_sub_num}sub/${trial_num}trials" | tee "./log/${model_name}/${sub_num}sub_${trial_num}trials.log"
+
+                echo "train ${model_name} with ${train_sub_num} subjects and ${trial_num} trials"
+
+                python ./../vicon_imu_data_process/augmentation_data.py --xrot_angle 0 --yrot_angle 0 --zrot_angle $rot_angle --rot_id "${rot_id}"
+
+                python main.py --config run.yaml --model_selection ${model_name} --feature_layer_num ${feature_layer_num} --cv_num ${cv_num} --tre_domain "${tre_data_relative_path}/${rot_id}rotid_${trial_num}trials_${sub_num}subjects_${base_name}" --tst_domain "${tst_data_relative_path}/${tst_data_relative_path}_${trial_num}trials_${sub_num}subjects_${base_name}" --sub_num ${sub_num} --trial_num ${trial_num}  --train_sub_num "${train_sub_num}" --test_sub_num ${test_sub_num} --config_name "${train_sub_num}sub_${trial_num}trials_${model_name}_${config_id}" --relative_result_folder "${relative_result_folder}/${rot_id}rotid/${train_sub_num}sub/${trial_num}trials" | tee "./log/${model_name}/${sub_num}sub_${trial_num}trials.log"
+
+                ./../batch_collect_test_files.sh "${relative_result_folder}/${rot_id}rotid"
+            done
+
         done
+    done
 else
     relative_result_folder="baseline_t1_5"
 
 fi
 
-./../batch_collect_test_files.sh "${relative_result_folder}"
+
+
+id"
 
--- batch_collect_test_files.sh
+++ batch_collect_test_files.sh
@@ -4,7 +4,7 @@
 if [ $# -gt 0 ]; then
     relative_path=$1
 else
-    relative_path='baseline_mlnn_v10_5'
+    relative_path='augmentation_v14'
 fi
 
 if [ $# -gt 1 ]; then
@@ -13,14 +13,16 @@ else
     filter_target='test_'
 fi
 
-collect_test_ile="${tflearning_path}/collect_test_files.sh"
+collect_test_file="${tflearning_path}/collect_test_files.sh"
 
 results_path="${MEDIA_NAME}drop_landing_workspace/results/training_testing"
-for train_sub_num in $(seq 2 15); do
-for trial_num in 25; do
-        #relative_path="augmentation_v6_${config_id}"
-        file_dir="$results_path/${relative_path}/${train_sub_num}sub/${trial_num}trials"
-        ${collect_test_ile} ${file_dir} ${filter_target}
+for train_sub_num in $(seq 1 15); do
+    for trial_num in $(seq 1 25); do
+        for rot_id in 5 10 15 30 50 75; do
+            #relative_path="augmentation_v6_${config_id}"
+            file_dir="$results_path/${relative_path}/${rot_id}rotid/${train_sub_num}sub/${trial_num}trials"
+            ${collect_test_file} ${file_dir} ${filter_target}
+        done
     done
 done
 
