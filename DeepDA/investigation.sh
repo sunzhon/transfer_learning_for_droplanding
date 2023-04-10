59caa6fd27baca152e4cc4e556a848879e0fb8b8
sunzhon <suntao.hn@gmail.com>
2023-04-10 17:07:50 +0200

    add more elaborate aug method

--- DeepDA/investigation.sh
+++ DeepDA/investigation.sh
@@ -9,30 +9,27 @@ sub_num=17
 cv_num=15  # cross validation num
 result_folder_array=()
 for rot_angle in 6 ; do
-    for train_sub_num in 3 7 9; do
+    for train_sub_num in 8; do
         test_sub_num=`expr ${sub_num} - ${train_sub_num}` 
         for trial_num in 30; do #10 15 20 25; do # NOTE: trial_num is only work for base_trail idx, 01, 02,.., not work on 01_0, 01_1,...
             for labels_name in "R_KNEE_MOMENT_X"; do # "R_KNEE_ANGLE_X" ; do
                 for model_name in  "baseline"; do # "augmentation"; do
-                    for dataset_name in "original" "rotation" "timewarp"; do #"timewarp"; do #"original" "rotation"; do #"rotation"; do # "rotation" "time_wrap"; do
+                    for dataset_name in "e_scale" "da_scale"; do #"da_scale" "da_rotation" "e_rotation"; do #"timewarp"; do #"original" "rotation"; do #"rotation"; do # "rotation" "time_wrap"; do
                         landing_manner="rdouble_leg"
-                        config_name="${model_name}_${landing_manner}_${dataset_name}_${labels_name}_${rot_angle}_${train_sub_num}_${trial_num}"
+                        config_name="${landing_manner}_${model_name}_${feature_layer_num}_${dataset_name}_${labels_name}_${rot_angle}_${train_sub_num}_${trial_num}"
 
                         # data_id="a3" #a3 no noise, mean r2=0.75, it is better than using noise.
                         # data_id="a4" #a4 no noise and node scale, mean r2=0.72
                         # data_id="tw1" # tw1, mean r2=0.79
-                        data_id="a5" # test_sub_num=14, mean r2 =  0.83 (rotation), 0.77 (original)
-
+                        data_id="a8" # test_sub_num=14, mean r2 =  0.83 (rotation), 0.77 (original)
 
                         echo "Start to train and test a model ......"
-
                         echo "dataset_name: ${dataset_name}"
                         echo "train subject num: ${train_sub_num}"
                         echo "test subject num: ${test_sub_num}"
                         echo "features_name: ${features_name}"
                         echo "labels_name: ${labels_name}"
                         sleep 1
-                        #echo "train ${model_name} with ${dataset_name} having ${train_sub_num} subjects and ${trial_num} trials"
 
                         data_folder="${dataset_name}"
                         tre_datafile_basename="tre_${data_id}_${landing_manner}_norm_landing_data.hdf5"
